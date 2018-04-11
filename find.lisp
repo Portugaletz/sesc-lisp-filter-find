6b8fc25d1935dfed92ff1983e125c055bcdab60a
Portugaletz <37464348+Portugaletz@users.noreply.github.com>
2018-04-11 23:01:05 +0700

    Rework done
    Oh yeah

--- find.lisp
+++ find.lisp
@@ -1,3 +1,3 @@
 (defun find-if1 (p s) (cond ((null s) s)
-                            ((p (car s)) (car s))
+                            ((funcall p (car s)) (car s))
                             (T (find-if1 p (cdr s)))))
